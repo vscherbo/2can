912ddc22eed883508fce32f28c1cbc5463b381be
Vladimir Scherbo <vscherbo@gmail.com>
2016-02-05 10:42:57 +0300

    initial

---
+++ http-server.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -
+
+#from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+import BaseHTTPServer
+import ssl
+import urllib
+import time
+import xml.etree.ElementTree as ET
+import psycopg2
+
+HOST_NAME = 'ct-apps01.arc.world' # !!!REMEMBER TO CHANGE THIS!!!
+PORT_NUMBER = 8123
+
+
+
+class HttpProcessor(BaseHTTPServer.BaseHTTPRequestHandler):
+    pg_srv = 'vm-pg'
+    def do_GET(s):
+        s.send_response(200)
+        s.send_header('content-type','text/html')
+        s.end_headers()
+        s.wfile.write("hello !")
+    def do_POST(s):
+        """Respond to a POST request."""
+        # Extract and print the contents of the POST
+        length = int(s.headers['Content-Length'])
+        #post_data = u''
+        #post_data += s.rfile.read(length).decode('utf-8')
+        post_data = s.rfile.read(length)
+        post_data=urllib.unquote_plus(post_data).replace("data=", "")
+        print "post_data="
+        print post_data #.encode('utf-8')
+        #root = ET.fromstring(post_data.encode('utf-8'))
+        root = ET.fromstring(post_data)
+        print u"tag=%s" % root.tag
+        print u"attrib=%s" % root.attrib
+ 
+
+        for child in root:
+            ins_dic = root.attrib.keys()
+            val_dic = root.attrib.values()
+            ins_str = u"INSERT INTO operations2can(" 
+            val_str = u' VALUES('
+            if 'Device' == child.tag:
+                dev_keys = []
+                for k in child.attrib.iterkeys():
+                    # add tag_name as a prefix to the key to make unique field_name
+                    dev_keys.append(child.tag+'_'+k)
+
+                ins_dic += dev_keys
+                val_dic += child.attrib.values()
+
+            ins_dic = ['description' if ('Reason' ==  fld) else fld for fld in ins_dic]
+            ins_str += u','.join(ins_dic)
+            val_dic = [u'NULL' if ( '' ==  v ) else u"'" + v + u"'" for v in val_dic]
+            val_str += ','.join(val_dic)
+
+            ins_str += u')'
+            val_str += u');'
+            sql_str = ins_str + val_str
+            # print "sql_str=%s" % sql_str
+            con_str = "host='" + s.pg_srv + "' dbname='arc_energo' user='arc_energo'" # password='XXXX' - .pgpass
+            # print "con_str=%s" % con_str
+            try:
+                con = psycopg2.connect(con_str)
+            except:
+                print "I am unable to connect to the database"
+
+            #TODO check return code
+            cur = con.cursor()
+            try:
+                cur.execute(sql_str)
+            except:
+                print "ERROR while INSERT"
+            con.commit()
+        
+        s.send_response(200)
+        s.send_header("Content-type", "text/html")
+        s.end_headers()
+
+if __name__ == '__main__':
+    server_class = BaseHTTPServer.HTTPServer
+    httpd = server_class((HOST_NAME, PORT_NUMBER), HttpProcessor)
+    # Example SSL: httpd.socket = ssl.wrap_socket (httpd.socket, certfile='path/to/localhost.pem', server_side=True)
+    print time.asctime(), "Server Starts - %s:%s" % (HOST_NAME, PORT_NUMBER)
+    try:
+            httpd.serve_forever()
+    except KeyboardInterrupt:
+            pass
+    httpd.server_close()
+    print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
---
+++ sender.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -
+
+import requests
+
+payment_xml=u"""<Payment Id="18" Amount="1200.00" CreatedAt="2013-09-30T13:34:42.5+04:00" RRN="111111111110" CardType="Visa" TID="" MID="" Card="424242** **** **** 4242" Description="" AuthCode="8e2a73" Status="Completed"><Device Id="66" Name="" Model="Galaxy Gio" /></Payment>"""
+
+void_xml=u"""<Payment Id="19" Amount="1200.00" CreatedAt="2013-09-30T13:36:35.6+04:00" RRN="111111111110" CardType="Visa" TID="" MID="" Card="424242** **** **** 4242" Description="" AuthCode="21eabd" Status="Voided"><Device Id="70" Name="" Model="Galaxy Gio" /></Payment>"""
+
+refund_xml=u"""<Refund Id="23" Amount="500.00" CreatedAt="2013-10-01T13:57:14.9+04:00" RRN="" CardType="Visa" TID="" MID="" Card="424242** **** **** 4242" Payment="22" Reason="Расторжение договора"><Device Id="84" Name="" Model="Galaxy Gio" /></Refund>"""
+
+
+real_2can="""data=%ef%bb%bf%3cPayment+Id%3d%221672518%22+Amount%3d%2220.00%22+CreatedAt%3d%222016-02-04T12%3a23%3a55.000%2b03%3a00%22+RRN%3d%22603512672518%22+CardType%3d%22Visa%22+TID%3d%2276004196%22+MID%3d%2276004196%22+Card%3d%22427685**+****+****+4779%22+Description%3d%22%d0%a2%d0%b5%d1%81%d1%82%22+AuthCode%3d%22116209%22+Status%3d%22Completed%22%3e%3cDevice+Id%3d%2259157%22+Name%3d%22%22+Model%3d%22Quest+410%22+%2f%3e%3c%2fPayment%3e"""
+
+#url_post = "http://paragpaph.ru:8123/post"
+#url_post = "http://217.150.43.11:8123/post"
+#url_post = "http://217.150.43.9:8123"
+
+url_post = "http://2can.kipspb.ru:8123/post"
+
+#r = requests.post(url_post, data = payment_xml)
+#r = requests.post(url_post, data = void_xml)
+r = requests.post(url_post, data = refund_xml.encode('utf-8'))
+
+print(r.status_code)
+
+                                                                                                                                                     
