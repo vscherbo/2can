5178e97aef99927bce9f52cadf5e734c911de7bc
Vladimir Scherbo <vscherbo@gmail.com>
2016-02-20 10:12:27 +0300

    заполнение bill_no из description

--- cr-fntr-op2can_comission.sql
+++ cr-fntr-op2can_comission.sql
@@ -6,6 +6,7 @@ CREATE OR REPLACE FUNCTION fntr_op2can_comission()
   RETURNS trigger AS
 $BODY$BEGIN
    NEW.Sum = NEW.Amount - ROUND(NEW.Amount * 0.0275, 2);
+   NEW.bill_no = (REGEXP_REPLACE(COALESCE(NEW.description,'0'), '[^0-9]+', '', 'g'))::INTEGER;
    RETURN NEW;
 END;$BODY$
   LANGUAGE plpgsql VOLATILE
