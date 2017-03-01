a7601d8de22ee2b83037360418050b684e0a22ee
Vladimir Scherbo <vscherbo@gmail.com>
2017-03-01 12:42:18 +0300

    триггер BEFORE без условия WHEN

--- devel/cr-tr_op2can_BIU.sql
+++ devel/cr-tr_op2can_BIU.sql
@@ -1,10 +1,9 @@
 -- Trigger: tr_op2can_BIU on operations2can
 
--- DROP TRIGGER "tr_op2can_BIU" ON operations2can;
+DROP TRIGGER "tr_op2can_BIU" ON operations2can;
 
 CREATE TRIGGER "tr_op2can_BIU"
   BEFORE INSERT OR UPDATE
   ON operations2can
   FOR EACH ROW
-  WHEN (NEW.status NOT IN ('Voided', E'Отменен'))
   EXECUTE PROCEDURE fntr_op2can_comission();
