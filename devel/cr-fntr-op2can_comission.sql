be6f142802812ffc7603ded13fe3c762d07baa92
Vladimir Scherbo <vscherbo@gmail.com>
2017-03-01 12:44:13 +0300

    для всех операций и статусов

--- devel/cr-fntr-op2can_comission.sql
+++ devel/cr-fntr-op2can_comission.sql
@@ -5,19 +5,20 @@
 CREATE OR REPLACE FUNCTION fntr_op2can_comission()
   RETURNS trigger AS
 $BODY$BEGIN
-   IF NEW.tag = 'Refund' THEN
-      NEW.Sum = NEW.Amount;
-   ELSE
-      NEW.Sum = NEW.Amount - ROUND(NEW.Amount * 0.0275, 2);
-   END IF;
+IF NEW.tag = 'Payment' AND NEW.status IN ('Completed', 'Проведен')
+THEN
+    NEW.Sum = NEW.Amount - ROUND(NEW.Amount * 0.0275, 2);
+ELSE -- Refund or Voided
+    NEW.Sum = NEW.Amount;
+END IF;
    
-   BEGIN
-      -- NEW.bill_no = (REGEXP_REPLACE(COALESCE(NEW.description,'0'), '[^0-9]+', '', 'g'))::INTEGER;
-      NEW.bill_no = (REGEXP_REPLACE(NEW.description, '[^0-9]+', '', 'g'))::INTEGER;
-   EXCEPTION WHEN OTHERS THEN
-      NEW.bill_no = NULL;
-   END;
-   RETURN NEW;
+BEGIN
+    NEW.bill_no = (REGEXP_REPLACE(NEW.description, '[^0-9]+', '', 'g'))::INTEGER;
+EXCEPTION WHEN OTHERS THEN
+    NEW.bill_no = NULL;
+END;
+
+RETURN NEW;
 END;$BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
